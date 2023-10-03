0e32c74f132347efab12a5e7565d6bfab0cefc7f
gilly <nick.gilmore20@gmail.com>
2023-10-03 12:26:48 -0500

    Initial Commit

--- src/main/java/Person.java
+++ src/main/java/Person.java
@@ -1,33 +1,6 @@
 
-//-- Creating a class named Person,
-//   Creating a blueprint for our object
-
 public class Person {
 
-    //-- Static Property
-    public static long worldPopulation = 7_500_000_000L;
-    //-- Instance Property
-    public String name;
-
-
-
-    public static void main(String[] args) {
-
-    Person theBestDrummerAlive = new Person();
-    theBestDrummerAlive.name = "Neil Peart";
-
-    Person.worldPopulation += 1;
-
-        System.out.println(Person.worldPopulation);
-
-        // Technically will work, however it's better to reference
-        // static properties through the class, not instances
-        System.out.println(theBestDrummerAlive.worldPopulation);
-
-        //-- Error will occur
-        System.out.println(Person.name);
-
-    }
 
 
 }
