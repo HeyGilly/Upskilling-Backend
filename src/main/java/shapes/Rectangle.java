67ae2dbb92a0afea42297fd7ea222e051fee2c71
gilly <nick.gilmore20@gmail.com>
2023-10-06 16:00:46 -0500

    Inheritance and Polymorphism: Exercise Completed

--- src/main/java/shapes/Rectangle.java
+++ src/main/java/shapes/Rectangle.java
@@ -5,19 +5,20 @@ public class Rectangle {
     protected int length;
     protected int width;
 
+    //Constructor
     public Rectangle(int length, int width){
         this.length = length;
         this.width = width;
     }
 
-    public int getArea(int length, int width){
+    public int getArea(){
         //area = length x width
-        return length * width;
+        return this.length * this.width;
     }
 
-    public int getPerimeter(int length, int width){
+    public int getPerimeter(){
         //perimeter = 2 x length + 2 x width
-        return (2*length) + (2* width);
+        return (2* this.length) + (2* this.width);
     }
 
 
--- src/main/java/shapes/Square.java
+++ src/main/java/shapes/Square.java
@@ -2,8 +2,23 @@ package shapes;
 
 public class Square extends Rectangle {
 
+    //constructor
     public Square(int sides){
-
+        super(sides, sides);
     }
+
+    // Override
+    @Override
+    public int getArea(){
+        //area = length x width
+        return length * length;
+    }
+
+   @Override
+    public int getPerimeter() {
+        //perimeter = 2 x length + 2 x width
+       return 4 * width;
+   }
+
 
 }
