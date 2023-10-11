9e9052564fb92517150e57b932e5a7e652e33047
gilly <nick.gilmore20@gmail.com>
2023-10-11 15:03:23 -0500

    Collections Exercise: Basic Exercise Complete

--- src/main/java/grades/GradesApplication.java
+++ src/main/java/grades/GradesApplication.java
@@ -32,35 +32,82 @@ public class GradesApplication {
         students.put("Chris_23", chris);
 
         Scanner scanner = new Scanner(System.in);
-        String anotherStudent;
+        String anotherStudent = null;
 
 
-        do {
             System.out.println("Welcome!");
-            System.out.println("Here are the GitHub usernames of our students:");
-            for (String name : students.keySet()) {
-                System.out.print(name + " | ");
-            }
+        do {
+            System.out.println("If you would like to view all students tpye '1' or if you would like to view each student type '2'");
+            int userOption = scanner.nextInt();
+
+
+            if (userOption != 1 && userOption != 2){
+
+            System.out.println("Please Insert the correct input");
+            System.out.println("\n\n****************");
+
+
+                anotherStudent = "y";
+            
+            }else if (userOption == 1){
+            System.out.println("\n****************");
+                for (String name : students.keySet()) {
+                    System.out.println("Name: "+students.get(name).getName()+" Github: "+name+" Average grade: "+students.get(name).getGradeAverage() );
+                }
+                scanner.nextLine();
+
+                System.out.println("****************\n");
+
+            System.out.println("Would you like to restart? y/n");
+                anotherStudent = scanner.nextLine();
+                
+            }else if (userOption == 2){
+
+            System.out.println("\n****************");
+                System.out.println("Here are the GitHub usernames of our students:");
+                for (String name : students.keySet()) {
+                    System.out.print(name + " | ");
+                }
 
             System.out.println("\nWhat student would you like to see more information on?");
-            String userInput = scanner.nextLine();
+                scanner.nextLine();
+                String userInput = scanner.nextLine();
 
-            if (students.containsKey(userInput)) {
+
+
+                if (students.containsKey(userInput)) {
+                    System.out.println("\n****************");
+
 //            Name: Zach - GitHub Username: zgulde
 //            Current Average: 87.4
-                //Find Student
+                    //Find Student
 
-                System.out.println("Name: " + students.get(userInput).getName() + " - Github Username: " + userInput);
-                System.out.println("Current Average: " + students.get(userInput).getGradeAverage());
-                System.out.println();
-                System.out.println("Would you like to see another student? y/n");
-                anotherStudent = scanner.nextLine();
-            } else {
-                System.out.println("Sorry, no student found with the GitHub username of" + userInput + ".");
-                System.out.println("Would you like to see another student? y/n");
-                anotherStudent = scanner.nextLine();
+                    System.out.println("Name: " + students.get(userInput).getName() + " - Github Username: " + userInput);
+                    System.out.println("Current Average: " + students.get(userInput).getGradeAverage());
+                    System.out.println();
+                    System.out.println("Would you like to see another student? y/n");
+                    anotherStudent = scanner.nextLine();
+                    System.out.println("\n\n****************");
+
+                } else {
+                    System.out.println("****************");
+
+                    System.out.println("Sorry, no student found with the GitHub username of " + userInput + ".");
+                    System.out.println("Would you like to see another student? y/n");
+                    anotherStudent = scanner.nextLine();
+                    System.out.println("****************\n");
+
+                }
+                
             }
+
+
         }while (anotherStudent.equalsIgnoreCase("y"));
+
+        System.out.println("Have a great day!");
+
+
+
 
     }
 }
