1d61b52de84aac36f2f59142e6f767050e2ebdaf
gilly <nick.gilmore20@gmail.com>
2023-10-12 16:54:46 -0500

    Annotations: Testing out the lecture ntoes that was provided, may not keep

---
+++ src/main/java/ExceptionsAndErrorHandling/TestingExceptions.java
@@ -0,0 +1,40 @@
+package ExceptionsAndErrorHandling;
+
+public class TestingExceptions {
+    public static void main(String[] args)
+    throws  Exception{  // THis is added after i added the throw new exception
+
+
+        // Errors = not really our capability to deal with
+
+        //Exceptions, we can work with - we can look at checking for them (while compiling) or attempt to provide structures to find them
+
+
+
+
+
+
+
+//        Example:
+
+        int[] nums = {1,2,3};
+        System.out.println(nums[999]);
+
+
+        //you can tell Java to THROW an exception
+
+        System.out.println("I did hydrate - with Coffee");
+        throw new Exception("Fenrir Hydration Exception - did you hydrate?");
+
+
+//        try {
+//
+//        }catch ();
+
+
+
+
+
+
+    }
+}
