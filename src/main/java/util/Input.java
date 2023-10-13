5a2530999a44c88e3e901a2bf036c57bddcfa9f3
gilly <nick.gilmore20@gmail.com>
2023-10-13 14:44:49 -0500

    Exceptions and Error: Exercise completed

--- src/main/java/util/Input.java
+++ src/main/java/util/Input.java
@@ -3,7 +3,12 @@ package util;
 import java.util.Scanner;
 
 public class Input {
-    private static final Scanner sc = new Scanner(System.in);
+    private static Scanner sc;
+
+    public Input() {
+        sc = new Scanner(System.in);
+    }
+
 
     public boolean getString(){
         System.out.println("Please insert: Yes or No");
@@ -16,28 +21,44 @@ public class Input {
     }
 
     public int getInt(int min, int max){
-        int userInput = 0;
-        do{
-            System.out.println("Please insert a number between "+min+" and "+max);
-            userInput = sc.nextInt();
-        }while( min < userInput && userInput > max);
-        return userInput;
+
+          int userInput = 0;
+      try{
+          do{
+              System.out.println("Please insert a number between "+min+" and "+max);
+              userInput = Integer.valueOf(sc.nextInt());
+          }while( min < userInput && userInput > max);
+      }catch(Exception e) {
+          System.out.println("Uh oh, something went wrong: " + e.getMessage());
+          System.out.println("Here is some more detail:");
+          e.printStackTrace();
+      }
+          return userInput;
     }
 
     public int getInt(){
         return getInt(1, 10);
     }
 
-    public double getDouble(double min, double max){
+    public double getDouble(double min, double max) {
         double userInput = 0;
-        do{
-            System.out.println("Please insert a number between "+min+" and "+max);
-            userInput = sc.nextDouble();
-        }while( min < userInput && userInput > max);
-        return userInput;
-    }
+        do {
+            try {
 
-    public double getDouble(){
+                System.out.println("Please insert a number between " + min + " and " + max);
+                userInput = Double.valueOf(sc.nextDouble());
+
+            } catch (NumberFormatException e) {
+                System.out.println("Uh oh, something went wrong: " + e.getMessage());
+                System.out.println("Here is some more detail:");
+                e.printStackTrace();
+            }
+        } while (min < userInput && userInput > max);
+        return userInput;
+    };
+
+
+        public double getDouble(){
         return getDouble(1.00, 10.00);
     }
 
