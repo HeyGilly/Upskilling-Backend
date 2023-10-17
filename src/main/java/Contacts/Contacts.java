c49e304e546b3b53811cc1d9d2f08843d4bd6a0e
gilly <nick.gilmore20@gmail.com>
2023-10-17 10:52:49 -0500

    File IO: Pair Project Completed without Bonus

--- src/main/java/Contacts/Contacts.java
+++ src/main/java/Contacts/Contacts.java
@@ -1,5 +1,6 @@
 package Contacts;
 
+import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -13,47 +14,136 @@ import java.util.Scanner;
 public class Contacts {
     public static void main(String[] args) throws IOException {
 
+        Scanner scanner = new Scanner(System.in);
+
+/*
+    The application should be able to:
+         [ ] Show all your contacts
+         [ ] Add a new contact
+         [ ] Search a contact by her name
+         [ ] Delete an existing contact
+
+    * The application should keep its data in a file named contacts.txt
+
+    Bonus Help:
+        * When the application starts, the contact list should be read from the file.
+        * Before the application exits, the contacts file should be rewritten.
+
+ */
+
+        //-- Create a directory for data
+        Path dataDirectory = Paths.get("src/main/java/Contacts/data");
+        //-- Create a File to place the contacts information
+        Path contactFile = Paths.get("src/main/java/Contacts/data","contacts.txt");
+
+        //-- If Directory does not exist, Creat one.
+        if(Files.notExists(dataDirectory)){
+            Files.createDirectories(dataDirectory);
+            System.out.println("Data directory created successfully.");
+        }
+
+        //-- If contact File is not in dataDirectory, Create one.
+        if (Files.notExists(contactFile)){
+            //-- Creating File inside of Directory
+            Files.createFile(contactFile);
+            System.out.println("Contact file created successfully.");
+            //-- Creating a tittle for the File so this will already be made
+            List<String> contactsList = Arrays.asList("Name | Phone number", "---------------");
+            Files.write(contactFile, contactsList);
+            System.out.println("Tittle to contact list has been added. ");
+        }
+
+        System.out.println("Hello! Welcome to Contect Aplpication");
+        int userInput;
+        do{
+            //-- Showing the users options
+            System.out.println("1. View contacts.\n" +
+                    "2. Add a new contact.\n" +
+                    "3. Search a contact by name.\n" +
+                    "4. Delete an existing contact.\n" +
+                    "5. Exit.\n" +
+                    "Enter an option (1, 2, 3, 4 or 5):");
+            //-- Capturing the users option
+            userInput = scanner.nextInt();
 
 
-         /*
-            Contact Application:
+            //- Start of Condition
+            if (userInput == 1){
+                //-- Viewing All Contact
+                List<String> contactList = Files.readAllLines(contactFile);
+                for (String contact : contactList) {
+                    System.out.println(contact);
+                }
+                System.out.println();
+            }else if (userInput == 2){
+                scanner.nextLine();
+                //-- Add a new contact
+                System.out.println("Adding a new Contact:");
+                System.out.println("Please insert a new contact's name:");
+                String newUserName = scanner.nextLine();
+                System.out.println("Please insert a new contact's number: (Ex. 5556667777");
+                String newUserNumber = scanner.nextLine();
 
-                applciation should:
-                    [] Show all contacts
-                    [ ] Add new Contacts
-                    [ ] Search a contact by their name
-                    [ ] Delete an existing contact
+                //-- Add a line to an existing File
+                Files.write(
+                        Paths.get("src/main/java/Contacts/data","contacts.txt"),
+                        Arrays.asList(newUserName+" | "+newUserNumber),
+                        StandardOpenOption.APPEND
+                );
 
-                    [ ]  keep its data in a file named contacts.txt
+                //-- Update the new list so when you click on "1" it will show the new list
+                List<String> lines = Files.readAllLines(Paths.get("src/main/java/Contacts/data","contacts.txt"));
+                List<String> newList = new ArrayList<>();
+                newList.addAll(lines);
+                Files.write(Paths.get("src/main/java/Contacts/data","contacts.txt"), newList);
 
-                Keep in Mind:
-                    [ ] The text file should contain one contact per line.
-                    [ ] When the application starts, the contact list should be read from the file
-                    [ ] Before the application exits, the contacts file should be rewritten.
-                    [X] The user interface for your application should include a main menu like the following, where the user will need to enter a number between 1 and 5:
-                            1. View contacts.
-                            2. Add a new contact.
-                            3. Search a contact by name.
-                            4. Delete an existing contact.
-                            5. Exit.
-                            Enter an option (1, 2, 3, 4 or 5):
 
-                    [ ] The contacts information should be shown using the following format
-                            Name | Phone number
-                            ---------------
-                            Jack Blank | 1231231234
-                            Jane Doe | 2342342345
-                            Sam Space | 3453453456
+                System.out.println("You have added a new contact: " +newUserName+" | "+newUserNumber);
+                System.out.println();
+            } else if (userInput == 3) {
+                scanner.nextLine();
+                //-- Search for contact and give information
+                System.out.println("Please insert the name of the contact you are looking for:");
+                String searchUserName = scanner.nextLine();
+                //-- Message that will get send if No user of that name is found
+                String message = "Sorry we do not have a user of that name.";
+                //-- Loop through name and if name contains name then sout
+                List<String> lines = Files.readAllLines(Paths.get("src/main/java/Contacts/data","contacts.txt"));
+                for (String name : lines){
+                    if (name.contains(searchUserName)){
+                        //--Title
+                        message = "Name | Phone number\n";
+                        message += "---------------\n";
+                        message += name;
+                    }
+                }
+                System.out.println(message);
+                System.out.println();
+            } else if (userInput == 4) {
+                scanner.nextLine();
+                //-- Removing a certain user
+                //-- Search for contact and give information
+                System.out.println("Please insert the name of the contact you want to remove:");
+                String searchUserName = scanner.nextLine();
 
-                Here is what the code for your application might look like:
+                //-- Loop through name and if name contains name then skip
+                List<String> lines = Files.readAllLines(Paths.get("src/main/java/Contacts/data","contacts.txt"));
+                List<String> newList = new ArrayList<>();
+                for (String name : lines){
+                    if (name.contains(searchUserName)){
+                        System.out.println("Contact Removed: " + name);
+                        System.out.println();
+                        continue;
+                    }
+                    newList.add(name);
+                }
+                Files.write(Paths.get("src/main/java/Contacts/data","contacts.txt"), newList);
+            }else {
+                System.out.println("Please insert a correct option!");
+                System.out.println();
+            }
 
-                    [ ] Load all of the contacts by calling a method that returns a List of Contact objects.
-                    [ ] Call a method that shows the user the main menu and returns their choice of action.
-                    [ ] Using the user's choice from the previous step, call a method that performs that action (modifying the contents of the List of Contact objects if applicable).
-                    [ ] Keep calling the method from step two until the user chooses to exit.
-                    [ ] Once the user chooses to exit, re-write the contents of the contacts.txt file using the List of Contact objects.
-         */
-
+        }while(userInput != 5);
 
 
 
