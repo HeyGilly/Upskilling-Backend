1b727a22a83c9c3500bd196d4a2c535a2f93eb5a
gilly <nick.gilmore20@gmail.com>
2023-10-17 09:07:54 -0500

    File IO: Pair Project Restart

--- src/main/java/Contacts/Contacts.java
+++ src/main/java/Contacts/Contacts.java
@@ -14,11 +14,12 @@ public class Contacts {
     public static void main(String[] args) throws IOException {
 
 
-        /*
+
+         /*
             Contact Application:
 
                 applciation should:
-                    [X] Show all contacts
+                    [] Show all contacts
                     [ ] Add new Contacts
                     [ ] Search a contact by their name
                     [ ] Delete an existing contact
@@ -53,120 +54,11 @@ public class Contacts {
                     [ ] Once the user chooses to exit, re-write the contents of the contacts.txt file using the List of Contact objects.
          */
 
-        Scanner sc = new Scanner(System.in);
-
-
-        //-- Create a directory 'data'
-        //-- Create a file named 'contacts.txt'
-        Path filePath = Paths.get("contacts.txt");
-        Path directoryPath = Paths.get("src/main/java/data");
-        // Place File inside of Folder
-        Path fileInFolder = directoryPath.resolve(filePath);
-        //- Creates Directory if not exists
-        if(!Files.exists(directoryPath)){
-            Files.createDirectories(directoryPath);
-        }
-
-        // If there is not a file in directory then create file
-            Path contactPath = Paths.get("src/main/java/data", "contacts.txt");
-            List<String> readingContactList = Files.readAllLines(contactPath);
-
-
-
-
-        if(!Files.exists(fileInFolder)){
-            Files.createFile(fileInFolder);
-            // When application starts, The title of the file should be there.
-            List<String> contactsList = Arrays.asList("Name | Phone number", "---------------");
-            Files.write(contactPath, contactsList);
-            // When the application starts, the contact list should be read from the file
-
-        }
 
 
 
 
 
-
-
-        //-- Introducing the application
-        System.out.println("Hello! Welcome to contact application");
-        int userInput = 0;
-        do {
-            System.out.println();
-            System.out.println();
-
-            //-- Showing the users options
-            System.out.println("1. View contacts.\n" +
-                    "2. Add a new contact.\n" +
-                    "3. Search a contact by name.\n" +
-                    "4. Delete an existing contact.\n" +
-                    "5. Exit.\n" +
-                    "Enter an option (1, 2, 3, 4 or 5):");
-            //-- Capturing the users option
-            userInput = sc.nextInt();
-
-            //-- Condition on which the user inputs
-            if (userInput == 1) {
-                // View Contacts
-                for (String line : readingContactList) {
-                    System.out.println(line);
-                }
-            } else if (userInput == 2) {
-                sc.nextLine();
-                // Get users name,
-                System.out.println("Please Insert the new contact's name:");
-                String newContactName = sc.nextLine();
-                // Get users Number
-                System.out.println("Please Insert the new contact's number:");
-                String newContactNumber = sc.nextLine();
-                // Add to the existing file
-                Files.write(
-                        Paths.get("src/main/java/data", "contacts.txt"),
-                        Arrays.asList(newContactName + " | " + newContactNumber),
-                        StandardOpenOption.APPEND
-                );
-            } else if (userInput == 3) {
-                sc.nextLine();
-                System.out.println("What contact are you looking for?");
-                String searchedName = sc.nextLine();
-                System.out.println("Name | Phone number");
-                System.out.println("---------------");
-                for (String name : readingContactList) {
-                    if (name.contains(searchedName)) {
-                        System.out.println(name);
-                    }
-                }
-            } else if (userInput == 4) {
-                sc.nextLine();
-                System.out.println("What contact are you wanting to delete?");
-                String searchedName = sc.nextLine();
-
-
-                List<String> lines = Files.readAllLines(Paths.get("src/main/java/data", "contacts.txt"));
-                // Create an empty arraylist
-                List<String> updatedList = new ArrayList<>();
-
-                for(String line :lines){
-                    // If milk, then replace with cream
-                    if (!line.contains(searchedName)){
-                        updatedList.add(line);
-                        continue;
-                    }
-                }
-
-                Files.write(Paths.get("src/main/java/data", "contacts.txt"), updatedList);
-
-
-
-
-
-
-            }
-
-
-
-    } while (userInput != 5);
 
 
 
